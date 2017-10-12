71e49f4e4bbfef9361f7f84d61339e3259c8a3b3
PǝRV <alexander.w.matulionis@gmail.com>
2017-10-11 22:21:15 -0700

    NewImage and Move

--- dxweb.go
+++ dxweb.go
@@ -2,52 +2,98 @@ package dxweb
 
 import (
 	"sync"
-	"time"
 
 	"github.com/gopherjs/gopherjs/js"
 	"github.com/mrmiguu/jsutil"
+)
+
+const (
+	loadingms = 2500
 )
 
 var game *js.Object
 
 func init() {
 	<-jsutil.Load("assets/js/phaser.min.js")
-	game = js.Global.Get("Phaser").Get("Game").New(800, 600, nil, nil, js.M{"preload": preload, "create": create})
+	game = js.Global.Get("Phaser").Get("Game").New(800, 600, nil, nil, js.M{"create": create})
 }
 
-func preload() {
-}
+var ready = make(chan bool, 1)
+
+func run() { <-ready }
 
 func create() {
-	// game.Get("stage").Set("backgroundColor", "#182d3b")
 	game.Get("load").Get("onFileComplete").Call("add", fileComplete)
+	ready <- true
 }
 
-var x = 32
-var y = 80
+var start sync.Once
 
-var testl sync.Mutex
+var imagel sync.Mutex
+var images = map[string]*imageLoader{}
 
-func fileComplete(_, cacheKey *js.Object) {
-	go func() {
-		testl.Lock()
-		var newImage = game.Get("add").Call("image", x, y, cacheKey)
-		newImage.Set("alpha", 0)
-		fade := game.Get("add").Call("tween", newImage).Call("to", js.M{"alpha": 1}, 2500)
-		fade.Set("frameBased", true)
-		fade.Call("start")
-		newImage.Get("scale").Call("set", 0.3)
-		x += newImage.Get("width").Int() + 20
-		if x > 700 {
-			x = 32
-			y += 332
-		}
-		time.Sleep(1 * time.Second)
-		testl.Unlock()
-	}()
+type imageLoader struct {
+	wh <-chan [2]int
+	js chan<- *js.Object
 }
 
-func NewImage(url string) {
+func fileComplete(_, key *js.Object) {
+	obj := game.Get("add").Call("image", game.Get("world").Get("centerX"), game.Get("world").Get("centerY"), key)
+	obj.Set("alpha", 0)
+	obj.Get("anchor").Call("setTo", 0.5, 0.5)
+
+	imagel.Lock()
+	img := images[key.String()]
+	imagel.Unlock()
+	wh := <-img.wh
+	obj.Set("width", wh[0])
+	obj.Set("height", wh[1])
+	img.js <- obj
+
+	fade := game.Get("add").Call("tween", obj).Call("to", js.M{"alpha": 1}, loadingms)
+	fade.Set("frameBased", true)
+	fade.Call("start")
+}
+
+type Image struct {
+	key           string
+	width, height int
+	js            *js.Object
+}
+
+func NewImage(url string, width, height int) *Image {
+	start.Do(run)
+
 	game.Get("load").Call("image", url, url)
 	game.Get("load").Call("start")
+
+	wh := make(chan [2]int, 1)
+	wh <- [2]int{width, height}
+	obj := make(chan *js.Object, 1)
+	imagel.Lock()
+	images[url] = &imageLoader{wh, obj}
+	imagel.Unlock()
+
+	return &Image{
+		key:    url,
+		width:  width,
+		height: height,
+		js:     <-obj,
+	}
+}
+
+func (i *Image) Move(x, y int, ms ...int) {
+	if len(ms) > 1 {
+		panic("too many arguments")
+	}
+	millis := 1
+	if len(ms) > 0 {
+		if ms[0] < 1 {
+			panic("negative or zero ms")
+		}
+		millis = ms[0]
+	}
+	move := game.Get("add").Call("tween", i.js).Call("to", js.M{"x": x, "y": y}, millis)
+	move.Set("frameBased", true)
+	move.Call("start")
 }
