fb8d283fa25353f3f073d11fdefde4c724400fbf
PǝRV <alexander.w.matulionis@gmail.com>
2017-10-14 21:29:34 -0700

    Added audio

--- dxweb.go
+++ dxweb.go
@@ -8,8 +8,9 @@ import (
 )
 
 var (
-	Width  = 800
-	Height = 600
+	Width    = 800
+	Height   = 600
+	HitSound = ""
 
 	start sync.Once
 
@@ -19,17 +20,26 @@ var (
 
 	orderl sync.RWMutex
 	orders []order
-	objl   sync.Mutex
-	objs   = map[string]chan<- *js.Object{}
+
+	ldhit  sync.Once
+	hitsfx Sound
 )
 
 type order struct {
-	key string
-	ld  chan bool
+	key  string
+	keyc chan string
+	ld   chan bool
 }
 
 func init() {
 	style := js.Global.Get("document").Get("body").Get("style")
+
+	document := js.Global.Get("document")
+	meta := document.Call("createElement", "meta")
+	meta.Set("name", "viewport")
+	meta.Set("content", "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0")
+	document.Get("body").Call("appendChild", meta)
+
 	style.Set("background", "#000000")
 	style.Set("margin", 0)
 	<-jsutil.Load("assets/js/phaser.min.js")
@@ -42,40 +52,51 @@ func run() {
 	<-c
 
 	game.Get("canvas").Set("oncontextmenu", func(e *js.Object) { e.Call("preventDefault") })
-	mode := phaser.Get("ScaleManager").Get("SHOW_ALL")
+
 	scale := game.Get("scale")
+
+	window := js.Global.Get("window")
+	innerWidth, innerHeight := window.Get("innerWidth").Float(), window.Get("innerHeight").Float()
+	if Height > Width {
+		newWidth := (float64(Width) / float64(Height)) * innerHeight
+		scale.Call("setMinMax", newWidth, innerHeight, newWidth, innerHeight)
+	} else {
+		newHeight := (float64(Height) / float64(Width)) * innerWidth
+		scale.Call("setMinMax", innerWidth, newHeight, innerWidth, newHeight)
+	}
+
+	mode := phaser.Get("ScaleManager").Get("SHOW_ALL")
 	scale.Set("scaleMode", mode)
-	scale.Set("fullScreenScaleMode", mode)
 	scale.Set("pageAlignHorizontally", true)
 	scale.Set("pageAlignVertically", true)
+	scale.Call("refresh")
 
 	load = game.Get("load")
 	load.Get("onFileComplete").Call("add", func(_, key *js.Object) {
-		k := key.String()
-		var obj *js.Object
+		go func() {
+			k := key.String()
 
-		orderl.RLock()
-		for i, ord := range orders {
-			if ord.key != k {
-				continue
+			orderl.RLock()
+			for i, ord := range orders {
+				if ord.key != k {
+					continue
+				}
+				if i > 0 {
+					<-orders[i-1].ld
+				}
+				ord.keyc <- k
+				break
 			}
-			if i > 0 {
-				<-orders[i-1].ld
-			}
-			obj = game.Get("add").Call("image", game.Get("world").Get("centerX"), game.Get("world").Get("centerY"), k)
-			ord.ld <- true
-			break
-		}
-		orderl.RUnlock()
-		if obj == nil {
-			jsutil.Panic("object key not found")
-		}
-
-		objl.Lock()
-		objc := objs[k]
-		objl.Unlock()
-		objc <- obj
+			orderl.RUnlock()
+		}()
 	})
+}
+
+func loadHit() {
+	if len(HitSound) == 0 {
+		return
+	}
+	hitsfx = <-LoadSound(HitSound)
 }
 
 func tween(obj *js.Object, to js.M, ms ...int) {
@@ -109,39 +130,45 @@ type Image struct {
 
 func LoadImage(url string) <-chan Image {
 	start.Do(run)
+	ldhit.Do(loadHit)
 
+	ord := order{url, make(chan string, 1), make(chan bool, 1)}
 	orderl.Lock()
-	orders = append(orders, order{url, make(chan bool, 1)})
+	orders = append(orders, ord)
 	orderl.Unlock()
 	load.Call("image", url, url)
 	load.Call("start")
 
-	objc := make(chan *js.Object)
-	objl.Lock()
-	objs[url] = objc
-	objl.Unlock()
-
 	imgc := make(chan Image)
 	go func() {
-		hit := make(chan bool, 1)
-		img := Image{
+		obj := game.Get("add").Call("image", game.Get("world").Get("centerX"), game.Get("world").Get("centerY"), <-ord.keyc)
+		ord.ld <- true
+
+		obj.Set("alpha", 0)
+		obj.Get("anchor").Call("setTo", 0.5, 0.5)
+
+		hit := make(chan bool)
+		obj.Get("events").Get("onInputDown").Call("add", jsutil.F(func() {
+			if hitsfx.js != nil {
+				hitsfx.Play()
+			}
+			hit <- true
+		}))
+
+		imgc <- Image{
 			Hit: hit,
 			key: url,
-			js:  <-objc,
+			js:  obj,
 		}
-		img.js.Set("alpha", 0)
-		img.js.Get("anchor").Call("setTo", 0.5, 0.5)
-		img.js.Get("events").Get("onInputDown").Call("add", func() { hit <- true })
-		imgc <- img
 	}()
 	return imgc
 }
 
-func (i *Image) Pos() (int, int) {
+func (i Image) Pos() (int, int) {
 	return i.js.Get("x").Int(), i.js.Get("y").Int()
 }
 
-func (i *Image) Size() (int, int) {
+func (i Image) Size() (int, int) {
 	return i.js.Get("width").Int(), i.js.Get("height").Int()
 }
 
@@ -163,4 +190,36 @@ func (i *Image) Show(b bool, ms ...int) {
 	if b {
 		i.js.Set("inputEnabled", true)
 	}
+}
+
+type Sound struct {
+	key string
+	js  *js.Object
+}
+
+func LoadSound(url string) <-chan Sound {
+	start.Do(run)
+
+	ord := order{url, make(chan string, 1), make(chan bool, 1)}
+	orderl.Lock()
+	orders = append(orders, ord)
+	orderl.Unlock()
+	load.Call("audio", url, url)
+	load.Call("start")
+
+	sfxc := make(chan Sound)
+	go func() {
+		obj := game.Get("add").Call("audio", <-ord.keyc)
+		ord.ld <- true
+
+		sfxc <- Sound{
+			key: url,
+			js:  obj,
+		}
+	}()
+	return sfxc
+}
+
+func (s Sound) Play() {
+	s.js.Call("play")
 }
